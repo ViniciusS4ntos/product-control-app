44de8069e17f9d3f99998bfaf32ab3d257f3dcac
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2025-12-09 10:45:50 -0300

    Instrucoes

--- src/br/com/productcontrol/main/App.java
+++ src/br/com/productcontrol/main/App.java
@@ -3,6 +3,7 @@ package br.com.productcontrol.main;
 public class App {
 
 	public static void main(String[] args) {
+		// Main
 		System.out.println("Sistema de controle de produtos iniciado!");
 
 	}
--- src/br/com/productcontrol/model/Produto.java
+++ src/br/com/productcontrol/model/Produto.java
@@ -1,10 +1,26 @@
 package br.com.productcontrol.model;
 
 public class Produto {
+//
+//É a representação de um produto do supermercado.
+//
+//Um model é só uma classe que guarda dados.
+//
+//📦 Produto.java
+//
+//Exemplo de atributos que um produto tem:
+	
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	// 📌 O que essa classe faz?
+//
+//✔️ Guarda dados
+//✔️ Tem getters e setters
+//✔️ Só representa o produto — não tem lógica de CRUD aqui
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+
 
 }
