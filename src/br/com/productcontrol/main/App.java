2ae21323c3cfb339cf9fae839723f1db3974765e
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2025-12-09 10:35:16 -0300

    Estruturas de pastas

---
+++ src/br/com/productcontrol/main/App.java
@@ -0,0 +1,10 @@
+package br.com.productcontrol.main;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Sistema de controle de produtos iniciado!");
+
+	}
+
+}
---
+++ src/br/com/productcontrol/util/InputUtil.java
@@ -0,0 +1,10 @@
+package br.com.productcontrol.util;
+
+public class InputUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
